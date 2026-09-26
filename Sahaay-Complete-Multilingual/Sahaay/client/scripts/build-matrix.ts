d335771ed89eff3a84f4843901192f386e9a0dc9
vani-1289 <vs8648711@gmail.com>
2026-09-26 13:03:50 +0530

    Remove duplicate project archive structure

--- Sahaay-Complete-Multilingual/Sahaay/client/scripts/build-matrix.ts
+++
@@ -1,6 +0,0 @@
-import { writeLocale, allKeys } from './locale-base.js';
-import { en } from '../src/locales/en.js';
-import { hi } from '../src/locales/hi.js';
-import { TranslationSchema } from '../src/locales/types.js';
-
-console.log('Generating comprehensive 21-language locale files...');
--- Sahaay-Complete-Multilingual/Sahaay/client/scripts/dump-keys.ts
+++
@@ -1,6 +0,0 @@
-import { en } from '../src/locales/en.js';
-import { hi } from '../src/locales/hi.js';
-
-const allKeys = Object.keys(en);
-console.log(`Total Keys: ${allKeys.length}`);
-console.log(JSON.stringify(allKeys, null, 2));
